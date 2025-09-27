e50d06fa5b0a1c4bfdc85f681173fd0297b02101
Vaggelis Karampoulis <vaggos971@gmail.com>
2025-09-27 17:13:10 +0300

    init all

---
+++ src/main/java/com/qualco/nations/NationsApplication.java
@@ -0,0 +1,13 @@
+package com.qualco.nations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qualco/nations/NationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qualco.nations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
